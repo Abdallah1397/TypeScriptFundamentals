4ec89a3b928fe802bb77262dc72013cf3d3285c0
Abdallah Mahmoud Ahmed Mahmoud Attallah <Abdallah.Attallah@Egypt.TE-Data.core>
2022-11-09 11:50:31 +0200

    Handle type annotaitons by using rest paramters with functions

--- dist/withFunctions.js
+++ dist/withFunctions.js
@@ -9,4 +9,10 @@ const welcomeMessage = (userName) => {
 console.log(welcomeMessage("Abdallah"));
 const showData = (name, city = "as", age) => `My Name is ${name} and I'm ${age} years old and I'm from ${city}`;
 console.log(showData("abdallah", undefined, 25));
+const summation = (...restParameter) => {
+    let result = 0;
+    restParameter.forEach((item) => (result += item));
+    return result;
+};
+console.log(summation(5, 6, 8, 9.2, 10));
 //# sourceMappingURL=withFunctions.js.map
--- src/typeAnnotations/withFunctions.ts
+++ src/typeAnnotations/withFunctions.ts
@@ -2,7 +2,7 @@
 // noImplicityAny became false to handle implicity error.
 // noImplicityReturn became true to give error if no implicity return
 // localVariableNotUsed  became true to give error if any loacal variable not used
-// noUnUsedParameters became true to give error if any paramter function not used
+// noUnUsedParameters became true to give error if any Parameter function not used
 
 // declare and define sayHello variable
 const sayHello = true;
@@ -17,9 +17,19 @@ const welcomeMessage = (userName: string): string => {
 
 console.log(welcomeMessage("Abdallah"));
 
-// Required, Optional and Default Paramter Value
-// Optional Paramter must be come after required paramters
+// Required, Optional and Default Parameter Value
+// Optional Parameter must be come after required Parameters
 const showData = (name: string, city: string = "as", age?) =>
   `My Name is ${name} and I'm ${age} years old and I'm from ${city}`;
 
-console.log(showData("abdallah",undefined, 25));
+console.log(showData("abdallah", undefined, 25));
+
+// Rest Parameter
+// summation function take rest parameter array of numbers and returns a number.
+const summation = (...restParameter: number[]): number => {
+  let result=0;
+  restParameter.forEach((item) => (result += item));
+  return result
+};
+
+console.log(summation(5,6,8,9.2,10));
