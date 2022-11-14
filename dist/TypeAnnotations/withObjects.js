b25ab7ca1ba400ffafcad8c412de90497171c040
Abdallah Mahmoud Ahmed Mahmoud Attallah <Abdallah.Attallah@Egypt.TE-Data.core>
2022-11-14 14:04:08 +0200

    Handle TypeAnnotations with objects

---
+++ dist/TypeAnnotations/withObjects.js
@@ -0,0 +1,7 @@
+"use strict";
+const obj = {
+    userName: "Abdallah",
+    age: 26,
+    isMale: true,
+};
+//# sourceMappingURL=withObjects.js.map
---
+++ src/typeAnnotations/withObjects.ts
@@ -0,0 +1,6 @@
+// Type Annotations with objects
+const obj: { userName: string; age: number; isMale: boolean } = {
+  userName: "Abdallah",
+  age: 26,
+  isMale: true,
+};
